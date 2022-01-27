800146c85fd9be7916ee9ef93533bf3918e406a2
Sean Breckenridge <seanbrecke@gmail.com>
2022-01-27 01:36:32 -0800

    add ci/mypy

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import io
 from typing import List
 from setuptools import setup, find_packages
 
-requirements: List[str] = []
-
 # Use the README.md content for the long description:
 with io.open("README.md", encoding="utf-8") as fo:
     long_description = fo.read()
@@ -21,14 +19,14 @@ setup(
     license="http://www.apache.org/licenses/LICENSE-2.0",
     packages=find_packages(include=[pkg]),
     package_data={pkg: ["py.typed"]},
-    install_requires=requirements,
     keywords="discord data",
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
